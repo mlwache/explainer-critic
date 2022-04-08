7b90d0b9f3a0f4cfecb54d0e2ad75c943f650d8e
mlwache <mlwache@users.noreply.github.com>
2022-04-08 17:59:30 +0100

    bring umap into function form

--- umap_plot.py
+++ umap_plot.py
@@ -1,26 +1,38 @@
 import torch
+from matplotlib.figure import Figure
 from torch import Tensor
 from utils import FastMNIST, set_device
 import umap
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set(context="paper", style="white")
 
-set_device()
-mnist = FastMNIST('./data', train=True, download=True)
+def show_umap_plot():
 
-reducer = umap.UMAP(random_state=42)
-data: Tensor = torch.flatten(mnist.data, start_dim=1, end_dim=3)
+    set_device()
+    mnist = FastMNIST('./data', train=True, download=True)
 
-print('computing embedding...')
-embedding = reducer.fit_transform(data)
-print('finished computing embedding')
+    sns.set(context="paper", style="white")
+    figure = get_umap_figure(mnist)
+    figure.show()
 
-fig, ax = plt.subplots(figsize=(12, 10))
-color = mnist.targets
-plt.scatter(embedding[:, 0], embedding[:, 1], c=color, cmap="Spectral", s=0.1)
-plt.setp(ax, xticks=[], yticks=[])
-plt.title("MNIST data embedded into two dimensions by UMAP", fontsize=18)
 
-plt.show()
+def get_umap_figure(dataset: FastMNIST) -> Figure:
+    reducer = umap.UMAP(random_state=42)
+    data: Tensor = torch.flatten(dataset.data, start_dim=1, end_dim=3)
+
+    print('computing embedding...')
+    embedding = reducer.fit_transform(data)
+    print('finished computing embedding')
+
+    fig, ax = plt.subplots(figsize=(12, 10))
+    color = dataset.targets
+    plt.scatter(embedding[:, 0], embedding[:, 1], c=color, cmap="Spectral", s=0.1)
+    plt.setp(ax, xticks=[], yticks=[])
+    plt.title("MNIST data embedded into two dimensions by UMAP", fontsize=18)
+
+    return fig
+
+
+if __name__ == '__main__':
+    show_umap_plot()
