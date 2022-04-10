354774ddcc3854a7bfe99ef166e8ad6a2e8bccbd
mlwache <mlwache@users.noreply.github.com>
2022-04-10 15:19:06 +0100

    Add UMAP embedding figures for input_x_gradients of different models

--- umap_plot.py
+++ umap_plot.py
@@ -1,24 +1,38 @@
+from typing import Tuple
+
 import torch
-from matplotlib.figure import Figure
+from matplotlib.figure import Figure, Axes
 from torch import Tensor
-from utils import FastMNIST, set_device
 import umap
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from evaluation_experiments_console import set_up_evaluation_experiments
+from utils import FastMNIST
+
 
 def show_umap_plot():
-
-    set_device()
-    mnist = FastMNIST('./data', train=True, download=True)
-
     sns.set(context="paper", style="white")
-    figure = get_umap_figure(mnist.data, mnist.targets)
-    figure.show()
+    print("Setting up experiments...")
+    explainers, test_loader, model_paths = set_up_evaluation_experiments(3, n_test_samples=10000)
+    for i in range(len(explainers)):
+        explanations, labels = explainers[i].get_labeled_explanations(test_loader, "input_x_gradient")
+        explanations = explanations.detach()
+        print(f"Embedding for model {model_paths[i]}:")
+        explanation_figure, exp_ax = get_umap_figure(explanations, labels)
+        exp_ax.set_title(f"Input x Gradient UMAP-Embedding of the model {model_paths[i]}", fontsize=18)
+        explanation_figure.show()
+
+    print("Embedding for the input:")
+    mnist = FastMNIST('./data', train=False, download=True)
+    input_figure, input_ax = get_umap_figure(mnist.data, mnist.targets)
+    input_ax.set_title("MNIST inputs embedded into two dimensions by UMAP", fontsize=18)
+    input_figure.show()
 
 
-def get_umap_figure(data: Tensor, labels: Tensor) -> Figure:
+def get_umap_figure(data: Tensor, labels: Tensor) -> Tuple[Figure, Axes]:
 
+    fig, ax = plt.subplots(figsize=(12, 10))
     reducer = umap.UMAP(random_state=42)
     data: Tensor = torch.flatten(data, start_dim=1, end_dim=3)
 
@@ -26,14 +40,12 @@ def get_umap_figure(data: Tensor, labels: Tensor) -> Figure:
     embedding = reducer.fit_transform(data)
     print('finished computing embedding')
 
-    fig, ax = plt.subplots(figsize=(12, 10))
     color = labels
-    ax.scatter(embedding[:, 0], embedding[:, 1], c=color, cmap="Spectral", s=0.1)
+    ax.scatter(embedding[:, 0], embedding[:, 1], c=color, cmap="Spectral", s=5)
     ax.set_xticks([])
     ax.set_yticks([])
-    ax.set_title("MNIST data embedded into two dimensions by UMAP", fontsize=18)
 
-    return fig
+    return fig, ax
 
 
 if __name__ == '__main__':
